b375eeb91ee434b3dcaf59200f482f0ddb367652
joe-boadi <joeboadi1@gmail.com>
2023-12-28 10:01:42 +0000

    Modify and update page content

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,11 @@ import Users from "./components/Users/Users";
 const Home = () => {
   return(
     <>
+      <h1 className="text-center text-2xl tracking-wide m-8">Albums</h1>
       <Albums/>
+      <h1 className="text-center text-2xl tracking-wide m-8">Posts</h1>
       <Posts/>
+      <h1 className="text-center text-2xl tracking-wide m-8">Users</h1>
       <Users/>
     </>
   );
