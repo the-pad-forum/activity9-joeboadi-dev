a4dc920adc91f5b15d8c8b293faf4ce3a89ab198
joe-boadi <joeboadi1@gmail.com>
2024-02-02 19:41:16 +0000

    fix: Add Tailwind CSS styling

--- app/components/Posts/Posts.tsx
+++ app/components/Posts/Posts.tsx
@@ -21,7 +21,7 @@ const Posts: FC = () => {
     <div>
       {posts.map(post => (
         <div key={post.id} className="grid grid-flow-row gap-6 place-content-center mb-7">
-          <div className='ring-2 ring-current border-black border-b-2 p-4'>
+          <div className='ring-2 ring-current border-base-content border-b-2 p-4'>
             <h3  className='text-xl'>{post.title}</h3>
             <p>{post.body}</p>
           </div>
