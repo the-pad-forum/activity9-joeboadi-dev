45f0bc9fd46c3e47a8ee4417804d6be5f9613bc7
joe-boadi <joeboadi1@gmail.com>
2024-02-04 18:55:12 +0000

    fix: Bug fix

--- app/components/Posts/Posts.tsx
+++ app/components/Posts/Posts.tsx
@@ -1,9 +1,5 @@
 import React, { useState, useEffect, FC } from 'react';
-<<<<<<< HEAD
-// import styles from './Posts.module.css';
-=======
 import styles from './Posts.module.css';
->>>>>>> edb3ec25efaa62384ab611bc1d57d83f6f676715
 
 interface Post {
   userId: number;
@@ -25,18 +21,16 @@ const Posts: FC = () => {
     <div>
       {posts.map(post => (
         <div key={post.id} className="grid grid-flow-row gap-6 place-content-center mb-7">
-<<<<<<< HEAD
-          <div className='ring-2 ring-current border-base-content text-base-content bg-base-200 border-b-2 p-4'>
-=======
-          <div className='ring-2 ring-current border-black border-b-2 p-4'>
->>>>>>> edb3ec25efaa62384ab611bc1d57d83f6f676715
-            <h3  className='text-xl'>{post.title}</h3>
-            <p>{post.body}</p>
+            <div className='ring-2 ring-current border-base-content text-base-content bg-base-200 border-b-2 p-4'>
+              <div className='ring-2 ring-current border-black border-b-2 p-4'>
+                <h3  className='text-xl'>{post.title}</h3>
+                <p>{post.body}</p>
+              </div>
           </div>
         </div>
       ))}
     </div>
-  );
-};
+  )
+}
 
 export default Posts;
