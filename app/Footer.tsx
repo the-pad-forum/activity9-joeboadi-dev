8112a5731c76ca7d6c926813fa8fff0903103b55
joe-boadi <joeboadi1@gmail.com>
2024-02-04 12:32:59 +0000

    Modify footer component

---
+++ app/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return(
+        <>
+            <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+                <aside>
+                    <p>Copyright © 2023 - All right reserved by Joe Boadi</p>
+                </aside>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
+
